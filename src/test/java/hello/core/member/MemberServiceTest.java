1b0a99969a47104d66d832c0cdac5cefed936cbb
geneaky <kaktus418@gmail.com>
2021-12-26 19:42:57 +0900

    [feat] dependency injection by appconfig

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class MemberServiceTest {
 
-  MemberService memberService = new MemberServiceImpl();
+  MemberService memberService =
 
   @Test
   public void join() throws Exception{
