aaa8295a4de0adb2fadd6d65e87c0b9a46d731e7
geneaky <kaktus418@gmail.com>
2021-12-26 20:11:01 +0900

    [refact] appConfig

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,12 +2,20 @@ package hello.core.member;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MemberServiceTest {
 
-  MemberService memberService =
+  MemberService memberService;
+
+  @BeforeEach
+  public void beforeEach(){
+    AppConfig appConfig = new AppConfig();
+    memberService = appConfig.memberService();
+  }
 
   @Test
   public void join() throws Exception{
