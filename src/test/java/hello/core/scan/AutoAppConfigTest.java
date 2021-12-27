1bf10368abfdfb3a495451ea7172ec7e94e8fcbd
geneaky <kaktus418@gmail.com>
2021-12-27 16:18:05 +0900

    [feat & test] component scan & autowired

---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,26 @@
+package hello.core.scan;
+
+import static org.assertj.core.api.Assertions.*;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+  @Test
+  public void basicScan() throws Exception{
+    //given
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+        AutoAppConfig.class);
+
+    MemberService memberService = ac.getBean(MemberService.class);
+    //when
+
+    //then
+    assertThat(memberService).isInstanceOf(MemberService.class);
+  }
+
+}
